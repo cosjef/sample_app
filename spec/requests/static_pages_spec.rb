3c9b1dfe98aef3c680492cb467cd5e65b1ed1e4d
Jeff Costa <cosjef@gmail.com>
2012-12-08 23:53:28 -0500

    After completing Exercise 3.5, #1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,5 +44,21 @@ describe "Home page" do
 	  end 	 
    end
 
+
+   describe "Contact page" do
+      it "should have the h1 'Contact'" do
+        visit '/static_pages/contact'
+          page.should have_selector('h1', :text => 'Contact')
+        end
+
+      it "should have the title 'Contact'" do
+         visit '/static_pages/contact'
+         page.should have_selector('contact',
+            :text => "Ruby on Rails Tutorial Sample App| Contact")
+       end
+     end
+
+   
+
 end
 
